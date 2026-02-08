98fba2f85e0beaff57f6e0c4b0068b10bd417fbf
Sayzana Kibru <kibrusayzana@gmail.com>
2026-02-08 19:12:47 +0300

    Create Single_Number.py

---
+++ Single_Number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        if len(nums)==1:
+            return nums[0]
+        nums.sort()
+        i=0 
+        j=1
+        while j<len(nums):
+            if nums[i]==nums[j]:
+                i+=2
+                j+=2
+            else:
+                return nums[i]
+        
+        return nums[-1]
